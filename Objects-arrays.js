365fcea8aa25bea5be694e9eaa9ae85c4969d23a
ahmedtaaw <ahmedtaaw@gmail.com>
2022-09-02 17:32:20 +0200

    Update Objects-arrays.js

--- Objects-arrays.js
+++ Objects-arrays.js
@@ -51,3 +51,41 @@ for(prop of Object.keys(sportsCar)){
 }
 //speed  : fast
 
+
+//*****////Tasks
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+function logDairy() {
+    for (var log of dairy) {
+        console.log(log)
+    }
+}
+logDairy()
+// Task 2
+const animal = {
+
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (key of Object.keys(bird)) {
+        console.log(key, ": "+bird[key])
+    }
+}
+birdCan()
+
+// Task 3
+
+function animalCan() {
+    for (prop in bird) {
+        console.log(prop,": "+bird[prop])
+    }
+}
+animalCan() 
