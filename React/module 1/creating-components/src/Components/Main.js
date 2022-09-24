6f732c22d2034702c106529a2c0a03c21c5b6d81
Ahmed Tawfik <Ahmed.Tawfik@itworx.com>
2022-09-24 22:03:35 +0200

    updates

--- React/module 1/creating-components/src/Components/Main.js
+++ React/module 1/creating-components/src/Components/Main.js
@@ -1,6 +1,8 @@
 import Intro1 from "./Intro1";
 import Intro2 from "./Intro2";
 
+import Card from "./Card";
+
 function Main(props){
     const styles={
         padding:"20px",
@@ -14,7 +16,9 @@ function Main(props){
                 <section style={styles}>
                     <Intro1 color="yellow" number="5"/>
                     <Intro2 friend="Peter"/>
-                    
+                    <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3" />
+        <Card h2="Third card's h2" h3="Third card's h3" />
                 </section>
         </main>
     )
