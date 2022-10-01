cf5cbac0d5203b991760a860767b5c67d18113e6
Ahmed Tawfik <Ahmed.Tawfik@itworx.com>
2022-10-02 00:34:23 +0200

    updates

---
+++ React/module 2/creating-components/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Btn from './Btn';
+
+function App() {
+  return (
+    <div className="App">
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
