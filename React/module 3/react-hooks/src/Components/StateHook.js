9509a4480230f5796d51395874f1119a6c1c9066
Ahmed Tawfik <Ahmed.Tawfik@itworx.com>
2022-10-30 20:08:13 +0200

    ftr/useState

---
+++ React/module 3/react-hooks/src/Components/StateHook.js
@@ -0,0 +1,67 @@
+import React,{useState,useCallback} from 'react';
+
+function StateHooks(){
+    const [count,setCount]=useState(0);
+    const [age, dayAfterDayAge]=useState(1);
+    const [students, schoolStudents]=useState([]);
+    const [todos,setTodos]=useState([]);
+    
+    const dayHuman=()=>{
+        dayAfterDayAge((a)=>a+1);
+    };
+
+    const addStudent=useCallback(()=>{
+        schoolStudents((t)=>[...t,"New Student"]);
+    });
+
+    const addTodos=(e)=>{
+        setTodos((d)=>[...d,e]);
+    };
+
+    const onPressEnter=(e)=>{
+        if(e.key==='Enter'){
+            addTodos(e.target.value);
+            e.target.value="";
+        }
+    };
+    
+    return(
+        <>
+            <p>you clicked {count} time</p>
+            <button onClick={()=>setCount(count+1)}>
+                Click me
+            </button>
+            <p>day after day Age: {age}</p>
+            <button onClick={dayHuman}>
+                click me for day after day Mr. Human
+            </button>
+            <hr/>
+            <p>School Students</p>
+            <button onClick={addStudent}>Add Student</button>
+            <table>
+                <tbody>
+                {students.map((student,index)=>{
+                return <tr  key={index}>
+                    <td>{student} with key {index}</td>
+                </tr>
+               
+            })}
+                </tbody>
+            </table>
+            <hr/>
+            <p>press enter to add todo</p>
+            <input  type="text" onKeyDown={onPressEnter} ></input>
+            <table>
+                <tbody>
+                {todos.map((todo,index)=>{
+                    return <tr key={index}>
+                        <td>{todo}</td>
+                    </tr>
+                })}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default StateHooks;
