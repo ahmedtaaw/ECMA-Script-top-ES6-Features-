db61450a783de1abf8011817071a8027d604ba31
Ahmed Tawfik <Ahmed.Tawfik@itworx.com>
2022-11-12 11:48:41 +0200

    quick-review-1

--- React/module 3/react-hooks/src/App.js
+++ React/module 3/react-hooks/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import StateHooks from '../src/Components/StateHook';
 import SimpleUseState from '../src/Components/SimpleUseState';
+import SimpleUseEffect from '../src/Components/SimpleUseEffect';
 function App() {
   return (
     <div className="App">
       {/**<StateHooks/>
-       **/}
+      
       <SimpleUseState/>
+      **/}
+      <SimpleUseEffect/>
     </div>
   );
 }
