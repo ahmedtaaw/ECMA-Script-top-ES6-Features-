1ebccb95e8ee75eecdb3eda3fb28a55f726885d7
Ahmed Tawfik <Ahmed.Tawfik@itworx.com>
2022-11-15 21:00:16 +0200

    call api fetch

--- quick-review-1/src/components/lev1-callAPI.js
+++ quick-review-1/src/components/lev1-callAPI.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from 'axios';
-const CallAPI= ()=> {
-    const [randomUserJsonData,setRandomUserJsonData]=useState('');
-    const fetchRandomData = () => {
-        return axios.get('https://randomuser.me/api')
-            .then(({data}) => {
-                // handle success
-                console.log(data);
-                return JSON.stringify(data,null,2);
-            })
-            .catch(err => {
-                // handle error
-                console.log(err);
-            })
-            .finally(function () {
-                // always executed
-            });
+const fetchRandomData = () => {
+    return axios.get('https://randomuser.me/api')
+        .then(({ data }) => {
+            // handle success
+            console.log(data);
+            return JSON.stringify(data, null, 2);
+        })
+        .catch(err => {
+            // handle error
+            console.log(err);
+        })
+        .finally(function () {
+            // always executed
+        });
 
-    };
-    useEffect(()=>{
-    fetchRandomData().then(randomdataz=>{
-        setRandomUserJsonData(randomdataz || 'no data back from API');
-    })
-    
-    },[])
+};
+const CallAPI = () => {
+    const [randomUserJsonData, setRandomUserJsonData] = useState('');
+   
+    useEffect(() => {
+        fetchRandomData().then(randomdataz => {
+            setRandomUserJsonData(randomdataz || 'no data back from API');
+        })
+
+    }, [])
     return (
         <div>
             <p>Fetch Random Data</p>
-            <button onClick={()=>{
+            <button onClick={() => {
                 fetchRandomData();
             }}>fetch Random Data</button>
             <p>{randomUserJsonData}</p>
